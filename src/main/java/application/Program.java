8b1fb303b5ef140e2714f06a749ad6e3818afb5d
gpego1 <gpego786@gmail.com>
2026-02-22 21:23:51 -0300

    feat: daoJDBC impl

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -1,16 +1,14 @@
 package application;
+import model.dao.DaoFactory;
+import model.dao.DepartmentDAO;
+import model.dao.SellerDAO;
 
-import model.entities.Department;
-import model.entities.Seller;
-
-import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
-        Department department = new Department(1, "BOOKS");
-        System.out.println(department);
 
-        Seller seller = new Seller(1, "Pego", "pego@email.com", new Date(), 3000.00, department);
-        System.out.println(seller);
+        SellerDAO sellerDAO = DaoFactory.createSellerDao();
+        DepartmentDAO departmentDAO = DaoFactory.createDepartmentDao();
+
     }
 }
---
+++ src/main/java/model/dao/DaoFactory.java
@@ -0,0 +1,16 @@
+package model.dao;
+
+
+import model.dao.impl.DepartmentDaoJDBC;
+import model.dao.impl.SellerDaoJDBC;
+
+public class DaoFactory {
+
+    public static SellerDAO createSellerDao() {
+        return new SellerDaoJDBC();
+    }
+
+    public static DepartmentDAO createDepartmentDao() {
+        return new DepartmentDaoJDBC();
+    }
+}
